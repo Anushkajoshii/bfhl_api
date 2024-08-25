6f3ba52fafe28f76bfde1c54b4aa006a4581f99c
Anushka Joshi <anushkajoshi020503@gmail.com>
2024-08-25 11:50:06 +0530

    final

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
+// Middleware to parse JSON bodies
 app.use(express.json());
 
 app.post('/bfhl', (req, res) => {
@@ -28,14 +30,12 @@ app.post('/bfhl', (req, res) => {
     });
 });
 
-
 app.get('/bfhl', (req, res) => {
     res.status(200).json({
         operation_code: 1
     });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
